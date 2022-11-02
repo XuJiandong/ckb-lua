ae26c08884669df902889ef0f152cb35f7226ceb
YI <uuuuuu@protonmail.com>
2022-11-02 10:18:26 +0800

    make printing hello world work

--- lua-loader/lua-loader.c
+++ lua-loader/lua-loader.c
@@ -443,20 +443,9 @@ int main(int argc, char **argv) {
     return (result && status == LUA_OK) ? 0 : -1;
 }
 
-__attribute__((visibility("default"))) int dylib_hello_world() { return 42; }
-
-/*
-** Main body of stand-alone interpreter (to be called in protected mode).
-** Reads the options and handles them all.
-*/
-static int pevaluate(lua_State *L) {
-    int ret = docall(L, 0, 0);
-    // Error happened, push no arguments to the stack.
-    if (ret) {
-        return 0;
-    }
-    lua_pushboolean(L, 1); /* signal no errors */
-    return 1;
+__attribute__((visibility("default"))) int dylib_hello_world() {
+    printf("hello world\n");
+    return 42;
 }
 
 // Running malloc inside the embedded lua instance may interfere with the
@@ -483,20 +472,12 @@ __attribute__((visibility("default"))) int evaluate_lua_code(void *l,
                                                              size_t code_size,
                                                              char *name) {
     lua_State *L = l;
-    int status, result;
     if (L == NULL) {
         l_message(name, "invalid lua state\n");
         return -1;
     }
-    status = luaL_loadbuffer(L, code, code_size, name);
-    if (status != LUA_OK) {
-        l_message(name, "loading code failed\n");
-        return -1;
-    }
-    lua_pushcfunction(L, &pevaluate); /* to call 'pmain' in protected mode */
-    status = lua_pcall(L, 0, 1, 0);   /* do the call */
-    result = lua_toboolean(L, -1);    /* get result */
-    return (result && status == LUA_OK) ? 0 : -1;
+    int status = dochunk(L, luaL_loadbuffer(L, code, code_size, name));
+    return status;
 }
 
 __attribute__((visibility("default"))) void close_lua_instance(void *L) {
